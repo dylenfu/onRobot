88a78fa06537823e31ffaf842906e3e65ebecbc2
dylenfu <dylenfu@126.com>
2020-10-19 18:01:07 +0800

    modify scripts

--- scripts/clear_nodes.sh
+++ scripts/clear_nodes.sh
@@ -2,8 +2,6 @@
 
 cd $PaletteWorkspace
 
-rm -rf node*
-
 for((i=$PaletteNodeIndexStart;i<=$PaletteNodeIndexEnd;i++)); do
     node="node$i"
     rm -rf $node
--- scripts/stop_nodes.sh
+++ scripts/stop_nodes.sh
@@ -3,6 +3,6 @@
 for((i=$PaletteNodeIndexStart;i<=$PaletteNodeIndexEnd;i++)); do
     p2pPort=$(($PaletteStartP2PPort+$i))
     echo "kill node$i, p2p port $p2pPort"
-    kill $(ps aux | grep geth | grep $p2pPort|grep -v grep | awk '{print $2}')
+    kill $(ps -ef|grep geth|grep $p2pPort|grep -v grep|awk '{print $2}')
     sleep 1s;
 done
