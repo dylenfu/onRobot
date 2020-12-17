5605f603adf73c036dfdc0dbaaa38073abbebfd8
dylenfu <dylenfu@126.com>
2020-12-17 17:27:41 +0800

    modify scripts

--- scripts/stop_node.sh
+++ scripts/stop_node.sh
@@ -9,7 +9,8 @@ if [[ $isRemote == "false" ]]
 then
     kill -INT `ps -ef|grep $identity|grep -v grep|awk '{print $2}'`;
 else
-    ssh -p 32000 ubuntu@$currentIp "pid=\$(ps aux | grep '$identity' | awk '{print \$2}' | head -1); echo \$pid |xargs kill";
+#    ssh -p 32000 ubuntu@$currentIp "pid=\$(ps aux | grep '$identity' | awk '{print \$2}' | head -1); echo \$pid |xargs kill";
+    ssh -p 32000 ubuntu@$currentIp "killall -9 geth";
 fi
 
 echo "kill $currentIp $identity";
