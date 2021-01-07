32e2d13193db00b45c5133ef5954e3799451aa76
dylenfu <dylenfu@126.com>
2021-01-07 14:38:13 +0800

    add some comments

--- cross.sh
+++ cross.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-make robot t=stop,clear
-make prepare
-make compile-dev
-make robot t=init,startGenesis,startValidator,addValidators
+# deploy eccd, eccm, ccmp contracts and record in config.json
 make robot t=deploy
+
+# bind ethereum plt asset and proxy address, set palette ccmp address in palette proxy(PLT contract)
 make robot t=bindProxy,bindAsset,ccmp
+
+# todo: poly dail failed
+# register side chain id to poly chain and approve it with 4 poly validators' wallet file.
 make robot t=registerSideChain,approveRegisterSideChain
+
+# sync palette header to palette chain and store poly book keepers in the palette chain
 make robot t=syncGenesis
