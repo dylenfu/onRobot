5fd24ffe1ae3aab258697503ce415775defe2f32
dylenfu <dylenfu@126.com>
2021-03-02 11:57:34 +0800

    temp

--- reset.sh
+++ reset.sh
@@ -11,3 +11,15 @@ make robot t=clear,init,start
 make robot t=eth-plt-mint-gov
 make robot t=eth-plt-mint-admin
 ./transfer_plt_ownership.sh
+
+make robot t=eth-eth-transfer
+make robot t=plt-lock,plt-unlock
+
+make robot t=plt-deploy-nft-asset
+make robot t=eth-deploy-nft-asset
+
+# 修改BindNFTAsset.json以及NFT-Lock.json&NFT-UnLock.json
+make robot t=plt-bind-nft-asset
+make robot t=eth-bind-nft-asset
+
+make robot t=nft-lock,nft-unlock
